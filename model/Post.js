57bbfa397951ff34b42ef97db05f57ce5f2f41b8
Huong <huongnguyenak96@gmail.com>
2019-05-06 18:25:04 +0700

    Post

--- model/Post.js
+++ model/Post.js
@@ -1,3 +1,14 @@
-
-
-
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const PostSchema = new Schema({
+    author: { type: Schema.Types.ObjectId, ref: 'user'},
+    content: String,
+    likes: [
+        { type: Schema.Types.ObjectId, ref: 'user'}
+    ],
+    comments: [
+        { type: Schema.Types.ObjectId, ref: 'comment'}
+    ]
+})
+const PostModel = mongoose.model('post',PostSchema);
+module.exports = { PostModel }
