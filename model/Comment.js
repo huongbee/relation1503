a8e2871cc53a6a2e6e57fff826de86f409e61549
Huong <huongnguyenak96@gmail.com>
2019-05-06 18:41:13 +0700

    Comment

--- model/Comment.js
+++ model/Comment.js
@@ -1,21 +1,27 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const CommentSchema = new Schema({
-    author: { type: Schema.Types.ObjectId, ref: 'user'},
-    post: { type: Schema.Types.ObjectId, ref: 'post'},
+    author: { 
+        type: Schema.Types.ObjectId, 
+        ref: 'user'
+    },
+    post: { 
+        type: Schema.Types.ObjectId, 
+        ref: 'post'
+    },
     content: String,
-    likes: [
-        { type: Schema.Types.ObjectId, ref: 'user'}
-    ],
-    replies:[
-        {
-            author: {type: Schema.Types.ObjectId, ref:'user'},
-            content: String,
-            likes: [
-                { type: Schema.Types.ObjectId, ref: 'user'}
-            ]
-        }
-    ]
+    likes: [{ 
+        type: Schema.Types.ObjectId, 
+        ref: 'user'
+    }],
+    replies:[{
+        author: {type: Schema.Types.ObjectId, ref:'user'},
+        content: String,
+        likes: [{ 
+            type: Schema.Types.ObjectId,
+            ref: 'user'
+        }]
+    }]
 })
 const CommentModel = mongoose.model('comment',CommentSchema);
 module.exports = { CommentModel }
