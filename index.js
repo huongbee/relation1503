ec4700815f1c3ebcaec02f1b4b4383ebaf1f2860
Huong <huongnguyenak96@gmail.com>
2019-05-08 18:41:41 +0700

    pull

--- index.js
+++ index.js
@@ -4,23 +4,51 @@ const { PostModel } = require('./model/Post');
 const { CommentModel } = require('./model/Comment');
 const { hash } = require('./lib/bcrypt')
 
-//4.3
-CommentModel.create({
-    author: '5cd0215a3bfbcc18d208c6b1', // manager
-    post: '5cd02834e608ba1fdbc4386e', // post 3
-    content: 'Manager Comment tren bai post so 3'
-})
-.then( cmt =>{
-    return PostModel.findOneAndUpdate({
-        _id: '5cd02834e608ba1fdbc4386e'
-    },{
-        $addToSet: {
-            comments: cmt._id
-        }
-    }, { new: true})
+UserModel.findOne({email: 'manager@gmail.com'})
+.then(user=>{
+    if(!user) return new Error('Cannot find user!')
+    return PostModel.findOneAndUpdate({_id:'5cd02834e608ba1fdbc4386e'},{
+        $pull: {
+            likes: user._id
+        } 
+    },{new: true})
 })
 .then(post=>console.log(post))
 .catch(err=>console.log({Error: err.message}))
+
+//4.4
+// UserModel.findOne({email: 'admin@gmail.com'})
+// .then(user=>{
+//     if(!user) return new Error('Cannot find user!');
+//     else{
+//         return PostModel.findOneAndUpdate({_id: '5cd02834e608ba1fdbc4386e'},{
+//             $addToSet: {
+//                 likes: user._id
+//             }
+//         }, {new: true})
+//     }
+// })
+// .then(post=>console.log(post))
+// .catch(err=>console.log({Error: err.message}))
+
+
+//4.3
+// CommentModel.create({
+//     author: '5cd0215a3bfbcc18d208c6b1', // manager
+//     post: '5cd02834e608ba1fdbc4386e', // post 3
+//     content: 'Manager Comment tren bai post so 3'
+// })
+// .then( cmt =>{
+//     return PostModel.findOneAndUpdate({
+//         _id: '5cd02834e608ba1fdbc4386e'
+//     },{
+//         $addToSet: {
+//             comments: cmt._id
+//         }
+//     }, { new: true})
+// })
+// .then(post=>console.log(post))
+// .catch(err=>console.log({Error: err.message}))
 
 //4.2
 // admin: 5cd020fd5ae5361868f3dfb5
